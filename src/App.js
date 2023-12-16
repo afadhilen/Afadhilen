3d0e973527f9c476cc88a4fc8c646726b1b8a3e3
Ahmad Fadhil Naswir <afadhilenproject@gmail.com>
2023-12-17 02:12:03 +0700

    Init landing page

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import Navbar from './components/Navbar'
+import Landing from './components/Landing'
+
+function App() {
+  return (
+    <div className="App font-mono">
+      <Navbar />
+      <Landing />
+    </div>
+  )
+}
+
+export default App
